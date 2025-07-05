4c60b929fb4c3fa1e124dc11ba2b7c38ccb8d441
head <head@headui-MacBookAir.local>
2025-07-06 01:27:42 +0900

    Add getUserProfile method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -25,54 +25,66 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    /*
-     * 회원가입
-     * 회원탈퇴
-     * 로그인
-     * 로그아웃
-     * 회원여부조회
-     * 
+    
+    /**
+     * 회원 가입 API
+     * POST /users
+     * @param user 가입할 사용자 정보
      */
-
     @PostMapping
-    public ResponseEntity<ApiResponse> create(@RequestBody User user) {
+    public ResponseEntity<ApiResponse> createUser(@RequestBody User user) {
         try {
             userService.signup(user);
 
             return new ResponseEntity<>(new ApiResponse("회원 가입이 성공적으로 완료되었습니다.", true), HttpStatus.CREATED);
         } catch( DuplicateEmailException e ) {
             // 이메일 중복 예외 처리
-            return new ResponseEntity<>(new ApiResponse("이미 가입된 이메일입니다.", false), HttpStatus.CONFLICT);
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.CONFLICT);
         } catch( Exception e ) {
             // 지정되지 않은 예외 발생
-            return new ResponseEntity<>(new ApiResponse("회원 가입이 실패하였습니다. 관리자에게 문의해주세요.", false), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/{email}")
-    public ResponseEntity<ApiResponse> delete(@PathVariable String email) {
-        try {
-            //userService.withdrawal(email);
-
-            return new ResponseEntity<>(new ApiResponse("회원 탈퇴가 성공적으로 완료되었습니다.", true), HttpStatus.OK);
-        } catch( UserNotFoundException e ) {
-            return new ResponseEntity<>(new ApiResponse("회원 정보가 존재하지 않습니다.", false), HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            // 지정되지 않은 예외 발생
-            return new ResponseEntity<>(new ApiResponse("회원 가입이 실패하였습니다. 관리자에게 문의해주세요.", false), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     /**
-     * TODO: 회원 정보 조회 API
-     * GET /users/{email}
-     * @param email 조회할 사용자 이메일
+     * 회원 탈퇴 API
+     * DELETE /users/{userId}
+     * @param userId 탈퇴할 사용자 아이디
+     */
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId) {
+        try {
+            //userService.withdrawal(userId);
+
+            return new ResponseEntity<>(new ApiResponse("회원 탈퇴가 성공적으로 완료되었습니다.", true), HttpStatus.OK);
+        } catch( UserNotFoundException e ) {
+            // 존재하지 않는 사용자
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.NOT_FOUND);
+        } catch( Exception e ) {
+            // 지정되지 않은 예외 발생
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 회원 정보 조회 API
+     * GET /users/{userId}
+     * @param userId 조회할 사용자 아이디
      * @return 사용자 정보
      */
     @GetMapping("/{userId}")
-    public ResponseEntity<Map<String, String>> getUserProfile(@PathVariable Long userId) {
-        Map<String, String> response = new HashMap<>();
-        return new ResponseEntity<>(response, HttpStatus.CONTINUE);
+    public ResponseEntity<ApiResponse> getUserProfile(@PathVariable Long userId) {
+        try {
+            User user = null;
+            //user = userService.getUserById(userId);
+
+            return new ResponseEntity<>(new ApiResponse("사용자 정보를 성공적으로 조회했습니다.", true, user), HttpStatus.OK);
+        } catch( UserNotFoundException e ) {
+            // 존재하지 않는 사용자
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.NOT_FOUND);
+        } catch( Exception e ) {
+            // 지정되지 않은 예외 발생
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -12,4 +12,10 @@ import lombok.AllArgsConstructor;
 public class ApiResponse {
     private String message;
     private boolean success;
+    private Object data;
+
+    public ApiResponse(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
 }
